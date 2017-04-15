50ac7fb5f3dc512ba3f080d4bee11ce3d7e563d2
anisham197 <anisha.m.197@gmail.com>
2017-04-15 12:38:47 +0530

    Lets make branches

--- app/queries.py
+++ app/queries.py
@@ -18,6 +18,8 @@ def getUserByUserID(user_id) :
 def getUser_ClassroomByCodeAndID(user_id, class_code) :
     return User_Classroom.query.filter(User_Classroom.user_id == user_id).filter(User_Classroom.class_code == class_code).first()
 
+
+#User_Classroom and Classroom Table
 def getJoinedClassroom(user_id, session) :
     return session.query(Classroom).join(User_Classroom, User_Classroom.class_code == Classroom.class_code).filter(User_Classroom.user_id == user_id).filter(User_Classroom.role == 1).all()
     #return session.query(Classroom, User_Classroom).filter(User_Classroom.user_id == user_id).filter(User_Classroom.role == 1).filter(User_Classroom.class_code == Classroom.class_code).all()
@@ -26,6 +28,7 @@ def getJoinedClassroom(user_id, session) :
 def getCreatedClassroom(user_id, session) :
     return session.query(Classroom).join(User_Classroom, User_Classroom.class_code == Classroom.class_code).filter(User_Classroom.user_id == user_id).filter(User_Classroom.role == 0).all()
 
+#fix it
 def getAssignmentByClassCode(class_code) :
     return Assignment.query.filter(Assignment.class_code == class_code).all()
 
