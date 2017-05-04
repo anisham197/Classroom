86a4b852dbb6e87f7bd75dcfbb400a52a89620de
anisham197 <anisha.m.197@gmail.com>
2017-05-04 13:51:38 +0530

    Refactor and Clean Up

--- app/classroom_module/controllers.py
+++ app/classroom_module/controllers.py
@@ -25,15 +25,10 @@ def index():
         role = getUserByUserID(session["user_id"]).role
         joined_classes = getJoinedClassroom(session["user_id"], db.session)
         created_classes = getCreatedClassroom(session["user_id"], db.session)
-            # x =[]
-            # for (class1 ,user_class) in joined_classes :
-            #     x.append(class1)
-            #     print ("\n\nyayyaya\n" + str(user_class.user_id) + "\n\n " + class1.subject)
         if role == 1 :
-            return render_template("classes/student_class.html", role=role, classes=joined_classes)
+            return render_template("classes/main_class.html", role=role, classes=joined_classes)
         else :
-            return render_template("classes/teacher_class.html", role=role, classes=created_classes)
-        # return render_template("classes/main_class.html", role=role, joined_classes=joined_classes, created_classes=created_classes)
+            return render_template("classes/main_class.html", role=role, classes=created_classes)
 
     if request.method == "POST" :
         # Check whether the entered class code is valid
@@ -47,7 +42,6 @@ def index():
                 return render_template("auth/no_access.html", msg="Already part of this class")
             else :
                 return render_template("auth/no_access.html", msg="You are creator of this class")
-
 
         # Add the student to the classroom
         user_class = User_Classroom( session["user_id"], request.form["class_code"], STUDENT)
@@ -63,8 +57,7 @@ def joined_classes():
         role = getUserByUserID(session["user_id"]).role
         joined_classes = getJoinedClassroom(session["user_id"], db.session)
         if role == 0 :
-            return render_template("classes/teacher_class.html", role=  role, classes=joined_classes)
-        # return render_template("classes/main_class.html", role=role, joined_classes=joined_classes, created_classes=created_classes)
+            return render_template("classes/main_class.html", role=  role, classes=joined_classes)
 
 @classroom_mod.route("/createclass", methods=["GET", "POST"])
 @login_required
